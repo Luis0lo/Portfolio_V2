1392b116818002df3e1ec85d8c104b6d55c957ab
Luis0lo <58235960+Luis0lo@users.noreply.github.com>
2022-09-01 12:31:31 +0100

    navbar created

--- src/App.js
+++ src/App.js
@@ -1,9 +1,11 @@
+
 import './App.css';
+import Navbar from './components/Navbar/Navbar';
 
 function App() {
   return (
     <div className="App">
-      <h1>Ready to begin</h1>
+      <Navbar/>
     </div>
   );
 }
