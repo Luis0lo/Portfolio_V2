61879dc37328e9a17d776d9afa0d04c0cb2fdf8a
Luis0lo <58235960+Luis0lo@users.noreply.github.com>
2022-09-02 10:28:27 +0100

    header section

--- src/sections/Home/Home.js
+++ src/sections/Home/Home.js
@@ -1,11 +1,30 @@
-import styles from './home.module.css'
+import styles from './home.module.css';
 
 const Home = () => {
-    return (
-        <section id='welcome' className={styles.container}>
-            Home
-        </section>
-    )
-}
+  return (
+    <section id="welcome" className={styles.container}>
+      <div className={styles.content}>
+        <div className={styles.description}>
+          <div className={styles.title}>
+            <h1>Luis Rodrigues</h1>
+            <h2>Full Stack Developer</h2>
+          </div>
+          <div className={styles.intro}>
+            <p>
+              I am in love with the endless possibilities that web development
+              gives us. Currently, I am focused on building projects using React
+              to make it second nature.
+            </p>
+            <br />
+            <p>Based in Manchester, UK.</p>
+          </div>
+        </div>
+        <div className={styles.devImage}>
+          <img src="/favicon.png" alt="" />
+        </div>
+      </div>
+    </section>
+  );
+};
 
-export default Home
+export default Home;
