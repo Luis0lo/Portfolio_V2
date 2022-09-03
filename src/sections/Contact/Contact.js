c2f5f61c0141fb43947d302e53426dc08ac6cc06
Luis0lo <58235960+Luis0lo@users.noreply.github.com>
2022-09-03 17:48:49 +0100

    form logic with Emailjs pack

--- src/sections/Contact/Contact.js
+++ src/sections/Contact/Contact.js
@@ -1,7 +1,36 @@
 import styles from './contact.module.css';
 import { FaEnvelopeOpenText, FaLinkedin } from 'react-icons/fa';
+import emailjs from 'emailjs-com';
+import Swal from 'sweetalert2';
 
 const Contact = () => {
+  const SERVICE_ID = process.env.REACT_APP_EMAILJS_SERVICE_ID;
+  const TEMPLATE_ID = process.env.REACT_APP_EMAILJS_TEMPLATE_ID;
+  const USER_ID = process.env.REACT_APP_EMAILJS_USER_ID;
+
+  const handleOnSubmit = (e) => {
+    e.preventDefault();
+    emailjs.sendForm(SERVICE_ID, TEMPLATE_ID, e.target, USER_ID).then(
+      (result) => {
+        console.log(result.text);
+        Swal.fire({
+          icon: 'success',
+          title: 'Message Sent Successfully',
+        });
+        e.target.reset();
+      },
+      (error) => {
+        console.log(error.text);
+        Swal.fire({
+          icon: 'error',
+          title: 'Ooops, something went wrong',
+          text: error.text,
+        });
+        e.target.reset();
+      }
+    );
+  };
+
   return (
     <section id="contact" className={styles.container}>
       <div className={styles.content}>
@@ -25,12 +54,16 @@ const Contact = () => {
             </div>
           </div>
           <div className={styles.contactForm}>
-            <form>
-              <input type="text" placeholder="Name" />
-              <input type="email" placeholder="Email" />
-              <textarea name="" id="" cols="30" rows="5">
-                Message
-              </textarea>
+            <form onSubmit={handleOnSubmit}>
+              <input name="name" type="text" placeholder="Name" required />
+              <input name="email" type="email" placeholder="Email" required />
+              <textarea
+                name="message"
+                cols="30"
+                rows="5"
+                placeholder="Message"
+                required
+              ></textarea>
               <button type="submit">Submit</button>
             </form>
           </div>
