3ca40577aa46ef30a9d7fc2015e0de52d3f2c4c8
Luis0lo <58235960+Luis0lo@users.noreply.github.com>
2022-09-09 15:22:52 +0100

    ignore social share component and uninstall react-share

--- src/components/SocialMediaShare/SocialMediaShare.js
+++ src/components/SocialMediaShare/SocialMediaShare.js
@@ -1,18 +1,18 @@
 
 import styles from './socialMediaShare.module.css';
-import { FaShareAlt, FaLinkedin, FaWhatsappSquare, FaTwitterSquare  } from 'react-icons/fa';
-import {WhatsappShareButton, LinkedinShareButton,TwitterShareButton} from 'react-share'
+// import { FaShareAlt, FaLinkedin, FaWhatsappSquare, FaTwitterSquare  } from 'react-icons/fa';
+// import {WhatsappShareButton, LinkedinShareButton,TwitterShareButton} from 'react-share'
 
 
 const SocialMediaShare = () => {
-const title = 'Luis is a Software Developer and has an interesting profile! It may fit your needs'
-const url = 'www.linkedin.com/in/withluis'
+// const title = 'Luis is a Software Developer and has an interesting profile! It may fit your needs'
+// const url = 'www.linkedin.com/in/withluis'
 
   return (
     <div className={styles.stickyShare}>
-        <p className={styles.shareIcon}><FaShareAlt/></p>
+        {/* <p className={styles.shareIcon}><FaShareAlt/></p> */}
 
-        <span>
+        {/* <span>
         <LinkedinShareButton
         title={title}
         summary='Luis is a Software Developer looking for an opportunity to step in the tech world'
@@ -35,7 +35,7 @@ const url = 'www.linkedin.com/in/withluis'
         url={url}>
             <FaWhatsappSquare className={styles.icons} title="Whatsapp"  />
         </WhatsappShareButton>
-        </span>
+        </span> */}
 
     </div>
   );
